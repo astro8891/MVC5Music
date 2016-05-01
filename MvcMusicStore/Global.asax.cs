e913a43f5b01bbe7502a398604f85d69cf2b635e
Ashley <Ashley>
2016-05-01 23:48:59 +1200

    added LocalDB config and additional models

--- MvcMusicStore/Global.asax.cs
+++ MvcMusicStore/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MvcMusicStore.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,6 +10,8 @@ namespace MvcMusicStore
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MusicStoreDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
